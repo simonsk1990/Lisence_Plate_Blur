b5b1473030417728686597f1f36778d4f9c1d4a5
Simon Shkilevich <sshkilevich@infinidat.com>
2020-03-29 14:24:56 -0700

    V1.1 Added return tuple values to detect_plate Added Median blur
    Final - Prod

--- main.py
+++ main.py
@@ -11,23 +11,35 @@ if img is None:
     print('failed to upload pic')
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
-
-
 #DETECT PLATE
 plate_cascade = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml')
 #using off the shelf haarcascade classifiers
 
-#plotting
+
+detected_img ,x,y,width,height= plate_detect(img, plate_cascade)
+#see plate_detect documntation
+
+#plotting detected plate
 plt.subplot(1,2,1)
-detected_img = plate_detect(img, plate_cascade)
 plt.imshow(detected_img)
-# plt.imshow(plate_detect(img,plate_cascade))
-plt.title('step 1 - detect licence plate ')
+plt.title('step 1 - detect licence plate using Cascade Classifier ')
+
+
+
 
 #BLUR PLATE
+ROI = img[y:y+height,x:x+width]
+ROI = cv2.medianBlur(ROI,7)
+#Will use Median blur with K=3 = lox pixel image
 
+#Create blurred pic
+blurred_licenced_plate = img.copy()
+blurred_licenced_plate[y:y+height,x:x+width] = ROI
 
-
+#plot blurred pic
+plt.subplot(1,2,2)
+plt.imshow(blurred_licenced_plate)
+plt.title('step 2 - Median Blurred licenced plate ')
 
 
 plt.show()
--- modules.py
+++ modules.py
@@ -9,7 +9,9 @@ def plate_detect(img,cascade_classifier):
     """
     :param img: src image
     :param cascade_classifier: var under cv2.CascadeClassifier
-    :return: img with rectangle over the features
+    :return: tuple[0] = img with rectangle over the features
+    :return: tuple[1] tuple[2]= x,y top left coordinates
+    :return: tuple[3] tuple[4]= width height
     """
     img_copy = np.copy(img)
     plate_cascade = cascade_classifier
@@ -18,4 +20,4 @@ def plate_detect(img,cascade_classifier):
     # receive x,y ,width, high
     for (x, y, width, height) in parameters:
         cv2.rectangle(img_copy, (x, y), (x + width, y + height), (255), 5)
-        return img_copy
+    return img_copy, parameters[0,0], parameters[0,1],parameters[0,2],parameters[0,3]
